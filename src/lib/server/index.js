7e50b4918c87154d347878093ddcf37d15632b3d
Nick Collings <nick@collingo.com>
2017-11-07 18:54:43 +0000

    Add PROTOCOL_DOMAIN to AUTH0 callback url

--- src/lib/server/index.js
+++ src/lib/server/index.js
@@ -34,7 +34,7 @@ module.exports = ({
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: '/callback'
+    callbackURL: `${process.env.PROTOCOL_DOMAIN}/callback`
   }, (accessToken, refreshToken, extraParams, profile, done) => {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
